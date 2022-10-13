efeab6bfac5e8992adcc4ef7e3df2f31b62ebe60
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2022-10-13 19:54:46 +0530

    main class functionality

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -2,9 +2,34 @@ package statisticker;
 
 import java.util.List;
 
-public class Statistics 
-{
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
-    }
+public class Statistics {
+	public static class Stats {
+		 Float max;
+		 Float min;
+		 Float average;
+	}
+
+	public static Stats getStatistics(List<Float> numbers) {
+		// implement the computation of statistics here
+		Stats statistics = new Stats();
+		int numberSize = numbers.size();
+		if (numberSize >= 1) {
+			float total = statistics.max = statistics.min = numbers.get(0);
+			for (int i = 1; i < numberSize; i++) {
+				float number = numbers.get(i);
+				if (number > statistics.max) {
+					statistics.max = number;
+				}
+				if (number < statistics.min) {
+					statistics.min = number;
+				}
+				total = total + number;
+			}
+			statistics.average = total / numberSize;
+		} else {
+			statistics.max = statistics.min = statistics.average = Float.NaN;
+		}
+		return statistics;
+	}
+
 }
